62783a24e184b79f4201ab2c5cd1ea083285cfec
ELDani45 <chilito45giron@gmail.com>
2025-06-01 17:41:28 -0500

    Hice un juego de adivinanza demasiado rapido. xd

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # proyecto.python1
 Proyectos de aprendizaje con python 
+
+# Nuevo EMPEZAR de proyectos python ahora con "GIT HUB" -- 
---
+++ juegoDEadivinanza.py
@@ -0,0 +1,24 @@
+
+print("Juego de adivinanza con Python.")
+print(" - Adivina el numero del 1 al 10. ")
+
+import random
+
+def adivinalo():
+    numeroAdivinar = random.randint(1,10)
+    numeroEncontrado = False
+    intentos = 1
+    while not numeroEncontrado:
+        numero = int(input("Escribe un numero: ").strip())
+        if numero > numeroAdivinar:
+            print(f" El numero {numero} es mayor al numero secreto. ")
+            intentos += 1
+        elif numero < numeroAdivinar:
+            print(f" El numero {numero} es menor al numero secreto.")
+            intentos += 1
+        else:
+            print(f"!Felicitaciones adivinastes el nuemro en {intentos} intentos¡.")
+            numeroEncontrado = True
+            break
+
+adivinalo()
